8ffabcb69db07589821c41b78b387e174fd6876a
Fernando Balieiro Ferreira <fernando.balieiro@outlook.com>
2024-06-24 15:59:56 -0300

    alterado a api do struct da conta

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"math/rand"
 	"time"
 )
 
@@ -8,7 +9,7 @@ type Account struct {
 	Id        uint64    `json:"id"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
-	Number    uint64    `json:"number"`
+	Number    int       `json:"number"`
 	Balance   float64   `json:"balance"`
 	CreatedAt time.Time `json:"created_at"`
 }
@@ -17,6 +18,7 @@ func NewAccount(firstName, lastName string) *Account {
 	return &Account{
 		FirstName: firstName,
 		LastName:  lastName,
+		Number:    int(rand.Int63n(10000)),
 		CreatedAt: time.Now().UTC(),
 	}
 }
